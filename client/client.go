c94ee9a0e90ad7cc210ab44581c73dc1c223ed69
Lorenz Thiede <lthiede@trustdble.com>
2024-05-07 16:52:49 +0200

    Implement produce path with grpc and protobuf

--- client/client.go
+++ client/client.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"crypto/sha1"
-	"encoding/base64"
-	"encoding/binary"
+	"context"
 	"fmt"
+	"io"
 	"log"
-	"net"
 	"sync"
 
-	"github.com/lthiede/cartero/connection"
-	"github.com/lthiede/cartero/messages"
+	pb "github.com/lthiede/cartero/proto"
+	"github.com/lthiede/cartero/server"
 	"go.uber.org/zap"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 )
+
+const numberClients int = 2
+const numberPartitions int = 4
 
 func main() {
 	logger, err := zap.NewDevelopment()
@@ -20,100 +24,90 @@ func main() {
 		log.Panicf("Error creating logger: %v", err)
 	}
 	defer logger.Sync()
-
-	var wg sync.WaitGroup
-	wg.Add(3)
-	for i := 0; i < 3; i++ {
-		go func(index int) {
-			var swg sync.WaitGroup
-			swg.Add(2)
-			conn, err := net.Dial("tcp", "localhost:8080")
-			if err != nil {
-				logger.Panic("Error starting connection", zap.Error(err))
-			}
-			defer conn.Close()
-			go produce(index, conn, &swg, logger)
-			go receiveAcks(index, conn, &swg, logger)
-			swg.Wait()
-			wg.Done()
-		}(i)
+	var clientWg sync.WaitGroup
+	clientWg.Add(numberClients)
+	for i := 0; i < numberClients; i++ {
+		go client(i, &clientWg, logger)
 	}
-	wg.Wait()
-	logger.Info("Client finished")
+	clientWg.Wait()
+	logger.Info("Clients finished")
 }
 
-func produce(index int, conn net.Conn, wg *sync.WaitGroup, logger *zap.Logger) {
-	defer wg.Done()
-	logger.Info("Start producing", zap.Int("index", index))
-	for i := 0; i < 5; i++ {
-		err := produceRequest(index, conn, fmt.Sprintf("partition%d", i%3), uint64(i), []uint32{3, 100, 50, 26}, logger)
-		if err != nil {
-			logger.Error("Error sending produce request", zap.Error(err))
-			return
-		}
-	}
-	logger.Info("Finished producing", zap.Int("index", index))
-}
-
-func produceRequest(index int, conn net.Conn, partition string, batchId uint64, messageLengths []uint32, logger *zap.Logger) error {
-	payloadLen := 0
-	for _, l := range messageLengths {
-		payloadLen += 4 + int(l)
-	}
-	partitionNameLen := 2 + len(partition)
-	requestLengthEncodingLen := 4
-	requestTypeEncodingLen := 1
-	batchIdEncodingLen := 8
-	var requestLen uint32 = uint32(requestTypeEncodingLen + partitionNameLen + batchIdEncodingLen + payloadLen)
-	request := make([]byte, 0, int(requestLen)+requestLengthEncodingLen)
-	request = binary.BigEndian.AppendUint32(request, uint32(requestLen))
-	request = append(request, connection.RequestTypeProduce)
-	request = binary.BigEndian.AppendUint16(request, uint16(len(partition)))
-	request = append(request, []byte(partition)...)
-	request = binary.BigEndian.AppendUint64(request, uint64(batchId))
-	for message, messageLength := range messageLengths {
-		request = binary.BigEndian.AppendUint32(request, messageLength)
-		for j := 0; j < int(messageLength); j++ {
-			request = append(request, byte(j*message*index))
-		}
-	}
-	if logger.Level() == zap.DebugLevel {
-		hasher := sha1.New()
-		hasher.Write(request[4:])
-		sha := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
-		logger.Debug("Write request", zap.String("sha", sha), zap.ByteString("request", request))
-	}
-	n, err := conn.Write(request)
+func client(i int, clientWg *sync.WaitGroup, logger *zap.Logger) {
+	defer clientWg.Done()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	conn, err := grpc.Dial("localhost:8080", opts...)
 	if err != nil {
-		return fmt.Errorf("error writing request to connection, wrote %d of %d bytes: %v", n, requestLen+uint32(requestLengthEncodingLen), err)
+		logger.Error("Failed to dial server", zap.Int("clientNumber", i), zap.Error(err))
+		return
 	}
-	return nil
+	defer conn.Close()
+	logger.Info("Dialed server", zap.Int("clientNumber", i))
+	var partitionWg sync.WaitGroup
+	if numberPartitions > 4 {
+		logger.Error("Too many partitions", zap.Int("numberPartitions", numberPartitions))
+		return
+	}
+	partitionWg.Add(numberPartitions)
+	for j := 0; j < numberPartitions; j++ {
+		go produce(i, fmt.Sprintf("partition%d", j), conn, &partitionWg, logger)
+	}
+	partitionWg.Wait()
+	logger.Info("Client finished", zap.Int("clientNumber", i))
 }
 
-func receiveAcks(index int, conn net.Conn, wg *sync.WaitGroup, logger *zap.Logger) {
-	defer wg.Done()
-	logger.Info("Start receiving acks", zap.Int("index", index))
-	for i := 0; i < 5; i++ {
-		response, err := messages.ProtocolMessage(conn, logger)
-		if err != nil {
-			logger.Error("Error reading response", zap.Error(err))
-			return
-		}
-		if response[0] == connection.ResponseTypeAckProduce {
-			logger.Info("Received ack")
-		} else {
-			logger.Error("Received unrecognized response type")
-			return
-		}
-		partition, bytesUsed, err := messages.NextString(response, logger)
-		if err != nil {
-			logger.Error("Error parsing partition name", zap.Error(err))
-		}
-		batchId, _, err := messages.NextUInt64(response[bytesUsed:])
-		if err != nil {
-			logger.Error("Error parsing batchId", zap.Error(err))
-		}
-		logger.Info("Received ack of batch", zap.Uint64("batchId", batchId), zap.String("partition", partition))
+func produce(i int, partitionName string, conn *grpc.ClientConn, partitionWg *sync.WaitGroup, logger *zap.Logger) {
+	defer partitionWg.Done()
+	client := pb.NewBrokerClient(conn)
+	md := metadata.New(map[string]string{server.PartitionNameMetadataKey: partitionName})
+	stream, err := client.Produce(metadata.NewOutgoingContext(context.Background(), md))
+	if err != nil {
+		logger.Error("Failed to issue produce", zap.Int("clientNumber", i), zap.String("partitionName", partitionName))
+		return
 	}
-	logger.Info("Finished receiving acks", zap.Int("Index", index))
+	logger.Info("Issued produce call", zap.Int("clientNumber", i), zap.String("partitionName", partitionName))
+	numberWritten := make(chan int)
+	numberAcks := 0
+	go produceMessages(stream, i, partitionName, numberWritten, logger)
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			logger.Error("Error receiving acks", zap.Error(err))
+			break
+		}
+		logger.Info("Received ack", zap.Int("batchId", int(in.BatchId)), zap.String("partitionName", partitionName))
+		numberAcks++
+	}
+	logger.Info("Finished produce call", zap.Int("clientNumber", i), zap.String("partitionName", partitionName), zap.Int("numberWritten", <-numberWritten), zap.Int("numberAcks", numberAcks))
+}
+
+var numberMessagesPerBatch []int = []int{3, 8, 2, 8}
+var message []byte = []byte{0, 6, 100, 23}
+
+func produceMessages(stream pb.Broker_ProduceClient, i int, partitionName string, numberWritten chan<- int, logger *zap.Logger) {
+	for batchNumber, numberMessages := range numberMessagesPerBatch {
+		messages := [][]byte{}
+		for n := 0; n < numberMessages; n++ {
+			messages = append(messages, message)
+		}
+		protoMessages := pb.Messages{
+			Messages: messages,
+		}
+		batch := pb.MessageBatch{
+			BatchId:  uint64(batchNumber + 1),
+			Messages: &protoMessages,
+		}
+		if err := stream.Send(&batch); err != nil {
+			logger.Error("Failed to send batch", zap.Int("clientNumber", i), zap.String("partitionName", partitionName), zap.Int("batchId", batchNumber+1), zap.Error(err))
+			stream.CloseSend()
+			numberWritten <- batchNumber
+			return
+		}
+		logger.Info("Produced batch", zap.Int("clientNumber", i), zap.String("partitionName", partitionName), zap.Int("batchId", batchNumber+1))
+	}
+	stream.CloseSend()
+	numberWritten <- len(numberMessagesPerBatch)
 }

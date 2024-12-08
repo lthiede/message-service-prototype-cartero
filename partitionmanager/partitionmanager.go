c4c05b44af08856ff647b460c6556eedc36c3a13
Lorenz Thiede <lorenz.07.lt@gmail.com>
2024-12-08 16:40:36 +0100

    Fix condition for assesing if partitions can be nicely divided among log nodes

--- partitionmanager/partitionmanager.go
+++ partitionmanager/partitionmanager.go
@@ -55,7 +55,7 @@ func (pm *PartitionManager) CreatePartition(partitionName string, numPartitions 
 }
 
 func (pm *PartitionManager) shittyLogNodeLoadBalancing(numPartitions uint32) ([][]string, error) {
-	if numPartitions*WriteQuorum%uint32(len(pm.logAddresses)) == 0 {
+	if numPartitions*WriteQuorum%uint32(len(pm.logAddresses)) != 0 {
 		return nil,
 			fmt.Errorf("creating a number of partitions that can't be evenly distributed on the log nodes is not supported. %d partitions, %d write quorum, %d log nodes",
 				numPartitions,
